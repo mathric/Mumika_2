12dd0ec93852acc97cd18eec44cd0220de47de6e
Mathric <mathric2015@gmail.com>
2022-12-15 14:29:18 +0800

    add image management app

---
+++ img_manage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImgManageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'img_manage'
---
+++ img_manage/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    parent_id = models.integerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class ImageInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    path = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag, related_name='images')
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
